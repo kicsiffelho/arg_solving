19a39a62f096b68725bf3d0314bf1943502bd8ab
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2024-03-01 17:04:13 +0100

    Add files via upload

---
+++ 03_Orkok (start)/03 - Orkok/Harcos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03___Orkok
+{
+    internal class Harcos : Ork
+    {
+        public Harcos(Fegyver fegyver) : this(null, fegyver)
+        {
+        }
+
+        public Harcos(string nev, Fegyver fegyver) : base(nev)
+        {
+            this.Eletero = 80;
+            this.Sebzes = 30;
+            this.Fegyver = fegyver;
+            this.Pajzs = 10;
+        }
+
+        public Fegyver Fegyver { get; set; }
+        
+        public int Pajzs { get; set; }
+
+        public new void Sebesul(int sebzes)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            string szoveg = base.ToString().Replace('}',' ');
+            szoveg += "pajzs: " + Pajzs + ", ";
+            szoveg += Fegyver;
+            return szoveg;
+        }
+    }
+}
---
+++ 03_Orkok (start)/03 - Orkok/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03___Orkok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Horda horda = new Horda();
+
+            Ork ork1 = new Ork();
+            Ork ork2 = new Ork("Gul'dan");            
+            horda.AddOrk(ork1);
+            horda.AddOrk(ork2);
+            horda.AddOrk(new Ork());
+            horda.AddOrk(new Ork("Dograh"));
+
+            foreach (Ork ork in horda.Orkok)
+            {
+                Console.WriteLine(ork);
+            }
+
+            Harcos harcos1 = new Harcos(new Fegyver("balta",15));
+            Harcos harcos2 = new Harcos("Deborah", new Fegyver("fejsze", 21));
+            Console.WriteLine(harcos1);
+            //Console.WriteLine(harcos2);
+
+            Console.ReadLine();
+        }
+    }
+}
